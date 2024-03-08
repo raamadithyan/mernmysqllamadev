1a2404700825e43a336f1d2df5f6a2f017f767ff
raamadithyan <chitha.don@gmail.com>
2024-03-08 21:54:53 +0530

    stuck at db post

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const app = express();
+app.use(express.json());
 
 const db = mysql.createConnection({
+	// host: "raamadithyan-mysql-db.a.aivencloud.com",
+	// user: "avnadmin",
+	// password: "AVNS_-NCTKXchy-enj7jMELw",
+	// database: "users",
 	host: "localhost",
 	user: "root",
 	password: "willpower",
+
 	database: "test",
 });
 
@@ -14,11 +20,23 @@ app.get("/", (req, res) => {
 	res.json("dala lama");
 });
 
+app.post("/books", (req, res) => {
+	const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
+	const value = ["Leena", "mycutie", "heart.jpg"];
+
+	db.query(q, [value], (err, data) => {
+		if (err) {
+			res.send(err);
+		}
+		return res.json(data);
+	});
+});
+
 app.get("/books", (req, res) => {
-	const q = " SELECT * FROM test.books";
+	const q = " SELECT * FROM books";
 	db.query(q, (err, data) => {
 		if (err) {
-			console.log("????");
+			res.send(err);
 		}
 		return res.json(data);
 	});
